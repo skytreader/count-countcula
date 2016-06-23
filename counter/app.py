a7fe6f9af827cbb23663f785227cb19faf8d4965
Chad Estioco <chadestioco@gmail.com>
2016-06-24 07:01:31 +0800

    set-up for keyboard event handling.

--- counter/app.py
+++ counter/app.py
@@ -7,7 +7,18 @@ from kivy.uix.widget import Widget
 Window.size = (600, 200)
 
 class CounterWidget(Widget):
-    pass
+    
+    def __init__(self, **kwargs):
+        super(CounterWidget, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._keydown_handler)
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._keydown_handler)
+        self._keyboard = None
+
+    def _keydown_handler(self, keyboard, keycode, text, modifiers):
+        pass
 
 class CounterApp(App):
     
